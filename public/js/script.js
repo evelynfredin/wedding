67e509f880874785674ec39d5143152866770e26
Evelyn Fredin <evefre2001@skola.goteborg.se>
2020-12-16 23:53:50 +0100

    Today is the best day ever and tomorrow's going to be even better

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,18 @@ var countDownDate = new Date("Feb 20, 2021 23:30").getTime();
 updateCountdown(); // Run once when the site is loaded.
 var timerFunction = setInterval(updateCountdown, 1000);
 
-var latestRandomIndex = 0;
+function buildDateString(distance) {
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  return `
+    <span class="countdown">${days}d</span>
+    <span class="countdown">${hours}h</span>
+    <span class="countdown">${minutes}m</span>
+  `;
+}
+
 function updateCountdown() {
   var now = new Date().getTime();
 
@@ -10,7 +21,7 @@ function updateCountdown() {
   var distance = countDownDate - now;
 
   // Display the result in the element with id="timer"
-  document.querySelector('.countdown').innerHTML = buildDateString(distance);
+  document.querySelector('.counter').innerHTML = buildDateString(distance);
 
   // If the count down is finished, write some text
   if (distance < 0) {
@@ -20,10 +31,4 @@ function updateCountdown() {
   }
 }
 
-function buildDateString(distance) {
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  return `${days}d ${hours}h ${minutes}m`;
-}
+
